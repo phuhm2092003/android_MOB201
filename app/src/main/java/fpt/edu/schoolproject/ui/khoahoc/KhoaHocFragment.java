f9c49560e22c4260832dd2afb029452f631dad1c
DELL <hominphuzxc@gmail.com>
2022-10-23 02:11:23 +0700

    Revert "video file"
    This reverts commit fcd3062d8c8f47b4b032a290445f273441d36902.

--- app/src/main/java/fpt/edu/schoolproject/ui/khoahoc/KhoaHocFragment.java
+++ app/src/main/java/fpt/edu/schoolproject/ui/khoahoc/KhoaHocFragment.java
@@ -12,7 +12,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.MediaController;
 import android.widget.VideoView;
 
 import fpt.edu.schoolproject.R;
@@ -43,15 +42,7 @@ public class KhoaHocFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        loadVideo();
         return view;
-    }
-
-    private void loadVideo() {
-        Uri uri = Uri.parse("android.resource://"+requireContext().getPackageName()+"/"+R.raw.video);
-        videoView.setVideoURI(uri);
-//        videoView.setMediaController(new MediaController(getContext()));
-        videoView.start();
     }
 
     private void initView(View view) {
@@ -64,6 +55,5 @@ public class KhoaHocFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        loadVideo();
     }
 }
